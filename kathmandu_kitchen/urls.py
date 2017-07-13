d0a0468d8496d4e3022a970f02048f77f9d5009b
Bibek Dahal <bibekdahal.bd16@gmail.com>
2017-07-13 16:48:04 +0545

    Create database models

--- kathmandu_kitchen/urls.py
+++ kathmandu_kitchen/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, static
 from django.contrib import admin
+from django.conf import settings
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^admin/', admin.site.urls),
-]
+] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ menu/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from branch.models import Branch
+
+
+class MenuCategory(models.Model):
+    branch = models.ForeignKey(Branch)
+    name = models.CharField(max_length=300)
+    image = models.FileField(upload_to='images', null=True, blank=True,
+                             default=None)
+
+    def __str__(self):
+        return self.name + ' (' + self.branch.name + ')'
+
+    class Meta:
+        verbose_name_plural = 'Menu categories'
+
+
+class FoodTag(models.Model):
+    name = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class FoodItem(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(MenuCategory)
+    tags = models.ManyToManyField(FoodTag, blank=True)
+    image = models.FileField(upload_to='images', null=True, blank=True,
+                             default=None)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
